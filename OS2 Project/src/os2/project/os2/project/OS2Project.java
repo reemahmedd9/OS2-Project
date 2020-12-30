5f86c4f3e2df180cd69ab18a13bf2078311ba09b
Hagar Atalla <hagar1800710@miuegypt.edu.eg>
2020-12-30 22:51:59 +0200

    ...

--- OS2 Project/src/os2/project/os2/project/OS2Project.java
+++ OS2 Project/src/os2/project/os2/project/OS2Project.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package os2.project;
+package os2.project.os2.project;
 
 import java.util.Scanner;
 
