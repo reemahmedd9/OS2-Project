2d89b3cae4b56828edd7e6ccb0a2f576294ac3e5
reemahmedd9 <74379045+reemahmedd9@users.noreply.github.com>
2020-12-30 22:12:46 +0200

    Revert "LRU"
    This reverts commit cb733f6099f8c65d1c4b5da4ddc0047e809e55ac.

--- OS2 Project/src/os2/project/os2/project/OS2Project.java
+++ OS2 Project/src/os2/project/os2/project/OS2Project.java
@@ -5,8 +5,6 @@
  */
 package os2.project;
 
-import java.util.Scanner;
-
 /**
  *
  * @author reema
@@ -18,52 +16,8 @@ public class OS2Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       System.out.print("Enter number of initially free frames:\t");
-       Scanner input = new Scanner(System.in);
-       int no_of_frames;
-       no_of_frames = input.nextInt();
-       System.out.print("Enter no. of page reference you want:\t");
-       int no_of_pages;
-        no_of_pages = input.nextInt();
-        int page_sequence[] = new int[no_of_pages];
-        System.out.println("Enter page reference sequence:");
-        for(int i = 0; i < no_of_pages; i++)
-        {
-            page_sequence[i] = input.nextInt();
-           
-        }
-        System.out.println();
-        int choice;
-        System.out.println("Choose the replacement algorithm");
-        System.out.println("Enter 1 for FIFO");
-        System.out.println("Enter 2 for LRU");
-        System.out.println("Enter 3 for Optimal");
-       choice = input.nextInt();
-       System.out.println();
-       System.out.println();
-        switch(choice) {
-   case 1:
-    FIFO fifo = new FIFO();
-    fifo.FIFO(no_of_frames, page_sequence);
-    break;
-  
-   case 2:
-    
-    LRU lru = new LRU();
-    lru.LRU(no_of_frames, page_sequence);
-    break;
-    
-   case 3:
-    
-    Optimal opt = new Optimal();
-    opt.Optimal(no_of_frames, page_sequence);
-    break;
-
-   default:
-      System.err.println("Enter correct choice");
-}
-   
-       
+        System.out.print("Reem");
+        System.out.print("hagar");
     }
     
 }
